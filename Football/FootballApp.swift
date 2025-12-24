a2c6d3ddcb9175e78689e4a147221230e8924a8d
Terence-PDX <61093911+Terence-PDX@users.noreply.github.com>
2025-12-24 13:46:05 -0800

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by TDodge47 on 2025-12-24.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
